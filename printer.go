29c5476cec145f125ebb5a07924407e5cd7747a5
george.smith <litesoft@gmail.com>
2023-07-24 10:58:38 -0700

    Update 2

--- printer.go
+++ printer.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 )
 
-// PrintString - Prints any string, adding a newLine
+// PrintString - Prints any string
 //
 //goland:noinspection ALL
 func PrintString(s string) {
+	fmt.Print(s)
+}
+
+// PrintStringln - Prints any string, adding a newLine
+//
+//goland:noinspection ALL
+func PrintStringln(s string) {
 	fmt.Println(s)
 }
 
-// PrintInt - Print any integer, adding a newLine
+// PrintInt - Print any integer,
 //
 //goland:noinspection ALL
 func PrintInt(i int) {
+	fmt.Print(i)
+}
+
+// PrintIntln - Print any integer, adding a newLine
+//
+//goland:noinspection ALL
+func PrintIntln(i int) {
 	fmt.Println(i)
 }
